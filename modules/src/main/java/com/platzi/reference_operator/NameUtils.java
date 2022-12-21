5ad692201d58cf7e5ae52458f7ae287f80a6a909
Luis Villalobos <luisevm24@outlook.com>
2022-12-20 21:02:56 -0600

    using method predefined by references operators

---
+++ modules/src/main/java/com/platzi/reference_operator/NameUtils.java
@@ -0,0 +1,17 @@
+package com.platzi.reference_operator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NameUtils {
+
+    @SafeVarargs
+    static <T> List<T> getList(T... elements) {
+        return Arrays.asList(elements);
+    }
+
+    public static void main(String[] args) {
+        List<String> teachers = getList("Luis", "Pedro", "Neon");
+        teachers.forEach(System.out::println);
+    }
+}
