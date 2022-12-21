ab22a30e69629e666c034c7c7c6c67883c98b5a7
Luis Villalobos <luisevm24@outlook.com>
2022-12-20 19:50:36 -0600

    learning predicates

--- modules/src/main/java/com/platzi/funtional/MathFunction.java
+++ modules/src/main/java/com/platzi/funtional/MathFunction.java
@@ -1,6 +1,7 @@
 package com.platzi.funtional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunction {
     public static void main(String[] args) {
@@ -14,6 +15,29 @@ public class MathFunction {
         System.out.println(squareFn.apply(25));
         System.out.println(square(10));
         System.out.println(square(20));
+        System.out.println();
+
+        Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        System.out.println("Is even 4? " + isEven.test(4));
+        System.out.println();
+
+        Predicate<Student> isApproved = student -> student.score >= 6;
+        System.out.println("Is approved the student 1? " + isApproved.test(new Student(5d)));
+        System.out.println("Is approved the student 2? " + isApproved.test(new Student(8.6d)));
+        System.out.println();
+    }
+
+    static class Student {
+        Double score;
+
+        public Student(Double score) {
+            this.score = score;
+        }
+
+        public Double getScore() {
+            return score;
+        }
     }
 
     static Integer square(int x) {
